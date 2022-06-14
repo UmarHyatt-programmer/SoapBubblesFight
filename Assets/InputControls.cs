754a5e5c4b7eb98e3edf9b74d88d7e06717e38fe
Umar <umarhyattoffice@gmail.com>
2022-06-14 22:32:55 +0500

    add resources

---
+++ Assets/InputControls.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+
+
+public class InputControls : MonoBehaviour
+{
+    
+    [SerializeField] bool FingerDown  = false;
+    [SerializeField] float MoveDeltaX = 0.0f;
+    private float LastPosX = 0.0f;
+    
+    public bool TouchDown   => FingerDown;
+    public float Horizontal => MoveDeltaX;
+
+    private void Start() => Input.multiTouchEnabled = false;
+    
+    private void Update()
+    { 
+        GetInput();
+    }//Update() end
+
+    private void GetInput()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            FingerDown = true;
+            LastPosX   = Input.mousePosition.x;
+        }//if end
+        else if (Input.GetMouseButton(0))
+        {
+            MoveDeltaX = Input.mousePosition.x - LastPosX;
+            LastPosX   = Input.mousePosition.x;
+        }//else if end
+        else if (Input.GetMouseButtonUp(0))
+        {
+            FingerDown = false;
+            MoveDeltaX = 0.0f;
+        }//else if end
+    }//GetInput() end
+
+}//class end
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] bool IsWalking     = false;
+    [SerializeField] float ForwardSpeed = 1.0f;
+    [SerializeField] float StrafeSpeed  = 1.0f;
+    [SerializeField] float ClampX       = 3.0f;
+
+    private InputControls Inputs  = null;
+    private Transform     Self    = null;
+    private Animator      Anim    = null;
+    private Rigidbody     RB      = null;
+    private Collider      Col     = null;
+    private float         InputX  = 0.0f;
+    private Vector3       MovePos = Vector3.zero;
+    Quaternion rot;
+    
+    private void Start() => Initialize();
+
+    private void Initialize()
+    {
+        Self   = transform;
+        // Anim   = GetComponent<Animator>();
+        RB     = GetComponent<Rigidbody>();
+        Col    = GetComponent<Collider>();
+        Inputs = GetComponentInChildren<InputControls>();
+    }//Initialize() end
+    
+    private void Update()
+    {
+        if(IsWalking==false)
+            IsWalking = Inputs.TouchDown;
+        // Anim.SetBool("Walk", IsWalking);
+        InputX = Inputs.Horizontal;
+    }//Update() end
+
+    private void FixedUpdate()
+    {
+        if(IsWalking)
+        {
+            MovePos   = Self.position + (new Vector3(InputX * StrafeSpeed, 0, ForwardSpeed * Time.deltaTime) * Time.deltaTime);
+            //MovePos.x = Mathf.Clamp(MovePos.x, -ClampX, ClampX);
+            rot = Quaternion.AngleAxis(InputX * StrafeSpeed,Vector3.up);
+            RB.MovePosition(MovePos);
+            //PlayerRotation();
+        }//if end
+        else
+            MovePos = Vector3.zero;
+    }//FixedUpdate() end
+
+    private float currentRotation;
+    void PlayerRotation()
+    {
+        currentRotation = Mathf.Atan2(InputX * StrafeSpeed, 1) * Mathf.Rad2Deg;
+        currentRotation = Mathf.Clamp(currentRotation, -30, 30);
+        var rotation = Quaternion.Euler(Vector3.up * currentRotation);
+        Debug.LogError(rotation);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2.5f);
+    }
+
+}//class end
